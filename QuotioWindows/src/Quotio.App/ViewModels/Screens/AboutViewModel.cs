3c32a67d18cf91e5af936ee02b94b1f1642f654e
dang232 <92773067+dang232@users.noreply.github.com>
2026-01-11 23:54:41 +0700

    Bind Windows About version to app constants

--- QuotioWindows/src/Quotio.App/ViewModels/Screens/AboutViewModel.cs
+++ QuotioWindows/src/Quotio.App/ViewModels/Screens/AboutViewModel.cs
@@ -1,12 +1,13 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Diagnostics;
+using Quotio.Core.Constants;
 
 namespace Quotio.App.ViewModels.Screens;
 
 public partial class AboutViewModel : ObservableObject
 {
-    public string Version => "1.0.0-windows-preview";
+    public string Version => $"{AppConstants.AppVersion}-windows-preview";
 
     [RelayCommand]
     private void OpenGitHub()
